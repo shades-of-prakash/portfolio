9b899d35163a18c868031281616a2e6ecce3135a
y22it058 <y22it058@rvrjc.ac.in>
2025-07-10 08:57:04 +0000

    blog component

--- src/components/BlogComponent.jsx
+++ src/components/BlogComponent.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const BlogComponent = () => {
-	return <div className="bg-white text-black">BlogComponent</div>;
+	return <div className="w-screen h-dvh bg-white text-black px-20">
+		<div className="h-full bg-red-900 flex flex-col">
+			<div className="w-full h-40 bg-blue-400"></div>
+		</div>
+	</div>;
 };
 
 export default BlogComponent;
