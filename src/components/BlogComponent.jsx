b8561f8683c76c8f8f1e09620858da94bf1603cd
y22it058 <y22it058@rvrjc.ac.in>
2025-07-10 09:12:43 +0000

    modified blog component

--- src/components/BlogComponent.jsx
+++ src/components/BlogComponent.jsx
@@ -2,8 +2,14 @@ import React from "react";
 
 const BlogComponent = () => {
 	return <div className="w-screen h-dvh bg-white text-black px-20">
-		<div className="h-full bg-red-900 flex flex-col">
-			<div className="w-full h-40 bg-blue-400"></div>
+		<div className="h-full bg-red-900 flex flex-col border border-black">
+			<div className="w-full h-40 bg-black flex justify-center items-center flex-col">
+				<div>
+					<span className="text-white">Prakash Varma's</span>
+					<p className="inline-block text-[100px] text-green-600 font-array">BLOG</p>
+				</div>
+			</div>
+			<div className="w-full flex-1 bg-white"></div>
 		</div>
 	</div>;
 };
