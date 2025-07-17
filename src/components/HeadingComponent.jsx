a837a6fce11c6474040afc457e95dabba8f0f108
prakashvarma00045 <prakashvarma00045@gmail.com>
2025-07-17 11:20:27 +0000

    marque

--- src/components/HeadingComponent.jsx
+++ src/components/HeadingComponent.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const HeadingComponent = ({ heading }) => {
 	return (
-		<div className="w-full h-auto flex items-center justify-center">
-			<p className="text-[50px] md:text-[100px] font-array text-accent">
+		<div className="w-full h-auto flex items-center justify-center  bg-gradient-to-l from-green-800/20 to-black">
+			<p className="text-[50px] md:text-[100px] font-array text-green-600">
 				{heading}
 			</p>
 		</div>
--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,32 +1,82 @@
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 import DADComponent from "./DADComponent";
 import ProjectComponent from "./ProjectComponent";
 import HeroComponent from "./HeroComponent";
 import HeadingComponent from "./HeadingComponent";
 import SkillsComponent from "./SkillsComponent";
-import ImageSlicer from "./ImageSlicer";
+import { HugeiconsIcon } from "@hugeicons/react";
+import { ArrowRight02Icon } from "@hugeicons/core-free-icons";
+
+gsap.registerPlugin(useGSAP);
 
 const MainLayout = () => {
-	const stripes = 20; // number of vertical stripes
-	// const imageUrl = "https://images.unsplash.com/photo-1485470733090-0aae1788d5af"// any image URL
-  	const imageUrl="https://4kwallpapers.com/images/walls/thumbs_3t/20421.jpg"
+	// useGSAP(() => {
+	// 	gsap.fromTo(
+	// 		trackRef.current,
+	// 		{ x: 0 },
+	// 		{
+	// 			x: -width,
+	// 			ease: "none",
+	// 			duration: 30,
+	// 			repeat: -1,
+	// 		}
+	// 	);
+	// });
+
 	return (
-		<div
-			className="bg-primary w-full h-screen overflow-y-scroll overflow-x-hidden"
-			id="main"
-		>
+		<div className="bg-primary w-full h-screen overflow-y-scroll overflow-x-hidden" id="main">
 			<HeroComponent />
 			<DADComponent />
 			<ProjectComponent />
-			{/* <div className="w-screen h-[800px] p-10 overflow-hidden">
-				<div className="w-full h-full bg-red-100 overflow-hidden">
-					<ImageSlicer
-					imageUrl={imageUrl}
-					numberOfSlices={stripes}
-					/>
+			<SkillsComponent />
+			<div className="w-screen overflow-hidden py-8 bg-green-600" >
+				<div
+					className="flex bg-red-900 justify-between whitespace-nowrap font-semibold text-xl text-black"
+				>
+					<div className="flex items-center gap-6  bg-blue-800 flex-1 items-center justify-center">
+						<span>Dynamic</span>
+					</div>
+					<HugeiconsIcon icon={ArrowRight02Icon} />
+					<div className="flex items-center gap-6  bg-blue-800 flex-1 items-center justify-center">
+						<span>Scalable</span>
+					</div>
+					<HugeiconsIcon icon={ArrowRight02Icon} />
+					<div className="flex items-center gap-6  bg-blue-800 flex-1 items-center justify-center">
+						<span>Responsive</span>
+					</div>
+					<HugeiconsIcon icon={ArrowRight02Icon} />
+					<div className="flex items-center gap-6  bg-blue-800 flex-1 items-center justify-center">
+						<span>Secure</span>
+					</div>
+					<HugeiconsIcon icon={ArrowRight02Icon} />
+					<div className="flex items-center gap-6  bg-blue-800 flex-1 items-center justify-center">
+						<span>Fast</span>
+					</div>
+					<HugeiconsIcon icon={ArrowRight02Icon} />
+					<div className="flex items-center gap-6  bg-blue-800 flex-1 items-center justify-center">
+						<span>Accessible</span>
+					</div>
+					<HugeiconsIcon icon={ArrowRight02Icon} />
+					<div className="flex items-center gap-6  bg-blue-800 flex-1 items-center justify-center">
+						<span>Interactive</span>
+					</div>
+					<HugeiconsIcon icon={ArrowRight02Icon} />
+
+					<div className="flex items-center gap-6  bg-blue-800 flex-1 items-center justify-center">
+						<span>User-Centric</span>
+					</div>
+					<HugeiconsIcon icon={ArrowRight02Icon} />
+
+					<div className="flex items-center gap-6  bg-blue-800 flex-1 items-center justify-center">
+						<span>Maintainable</span>
+					</div>
+					<HugeiconsIcon icon={ArrowRight02Icon} />
+
 				</div>
-			</div> */}
-			<SkillsComponent/>
+			</div>
+
 			<div className="w-screen h-dvh bg-red-900" id="about"></div>
 		</div>
 	);
